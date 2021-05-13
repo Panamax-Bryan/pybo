0446830e643396620dec78c80a861254b8fad127
hyungsub <koreanronin@gmail.com>
2021-05-13 13:31:45 +0900

    Add:pybo 앱 구현 - Modify: DB를 MySQL로 변경 - Add: pybo/models.py에 질문 모델 작성하기 - Add: pybo/models.py에 답변 모델 작성하기 - Add: 질문/ 답변 테이블 생성하기 - Modify: Question 모델 데이터 조회 결과에 속성값 보여 주기 - Add: 장고 Admin에 Question 모델 추가하기 - Add: 장고 Admin에 데이터 검색 기능 추가하기

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib     import Path
-from my_settings import SECRET_KEY
+from my_settings import DATABASES, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pybo',
 ]
 
 MIDDLEWARE = [
@@ -74,12 +75,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
