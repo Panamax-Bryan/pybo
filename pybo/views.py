91a496e177381d6c8dc096601520734cbb5a65c5
hyungsub <koreanronin@gmail.com>
2021-05-14 12:57:36 +0900

    Add: 오류 화면 구현하기

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,5 @@
 from django.http      import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models          import Question
 
 
@@ -16,6 +16,6 @@ def detail(request, question_id):
     """
     pybo 내용 출력
     """
-    question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context  = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
